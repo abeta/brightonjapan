e4fb8f350cd693555b799fbb04d45d4d5210abe0
koding user <adam@abemedia.co.uk>
2014-11-09 23:50:33 +0000

    update

--- _plugins/facebook.rb
+++ _plugins/facebook.rb
@@ -1,23 +1,25 @@
 module Jekyll
+
   class FacebookAlbum < Page
-    def initialize(site, base, dir, item)
+    def initialize(site, base, dir, name, category, data)
       @site = site
       @base = base
       @dir = dir
-      @name = 'index.html'
+      @name = name + '.html'
 
       self.process(@name)
       
       self.read_yaml(File.join(base, '_layouts'), 'gallery.html')
       
-      self.data['title'] = item['title']
-      self.data['image'] = item['image']
-      self.data['items'] = item['items']
-      self.data['category'] = 'gallery'
+      self.data['title'] = data['title']
+      self.data['image'] = data['image']
+      self.data['items'] = data['items']
+      self.data['category'] = category || 'gallery'
     end
   end
   
   class FacebookPhotosGenerator < Generator
+      
     def getdata(path)
       url = "http://graph.facebook.com/#{path}"
       json_data = Net::HTTP.get_response(URI.parse(url)).body
@@ -31,35 +33,39 @@ module Jekyll
       require 'json'
       
       username = Jekyll.configuration({})['profiles']['facebook'].split("/").last
-
-      entries = getdata(username + '/albums')
       
-      facebook = []
+      albums = getdata(username + '/albums')
       
-      for item in entries
-        unless item['count'].nil? || item['count'] == 0
+      for album in albums
+        unless album['count'].nil? || album['count'] == 0
           data = Hash.new
-          data['id'] = item['id']
-          data['title'] = item['name']
-          data['count'] = item['count']
-          data['image'] = getdata(item['cover_photo'])['source']
-          data['link'] = item['id']
+          data['id'] = album['id']
+          data['title'] = album['name']
+          data['count'] = album['count']
+          data['image'] = getdata(album['cover_photo'])['source']
+          data['link'] = album['id']
           
-          gallery = getdata(item['id'] + '/photos')
           data['items'] = []
-          for photo in gallery
-            data['items'].push(getdata(photo['id'])['source'])
+          photos = getdata(album['id'] + '/photos')
+          
+          for photo in photos
+            item = Hash.new
+            item['id'] = photo['id']
+            item['title'] = photo['name']
+            item['image'] = getdata(photo['id'])['source']
+            item['id'] = photo['id']
+            data['items'].push(item)
+            site.pages << FacebookAlbum.new(site, site.source, 'gallery/' + slug, 'index', data)
           end
           
           slug = data['title'].downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-          site.pages << FacebookAlbum.new(site, site.source, 'gallery/' + slug, data)
+          site.pages << FacebookAlbum.new(site, site.source, 'gallery/' + slug, 'index', data)
           
           facebook.push(data)
         end
-        
       end
-      
-      site.data['facebook_photos'] = facebook
     end
+    
   end
+  
 end
