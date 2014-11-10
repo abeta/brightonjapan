81924bfb5124800a2f4c80217e45d2cd27fef4ad
koding user <adam@abemedia.co.uk>
2014-11-10 03:27:46 +0000

    update

--- assets/app.js
+++ assets/app.js
@@ -46,6 +46,36 @@ $(function() {
     }); 
 });
 
+// equal heights
+function equalHeights() {
+	var $col = $('.equal>*'),
+		maxHeight = [],
+		rows = [],
+		rowTop = 0,
+		rowIndex = 0;
+
+	$col.each(function() {
+        $el = $(this);
+        $el.css('height', '');
+        if ($el.offset().top > rowTop) {
+            rowIndex++;
+            rows[rowIndex] = [];
+            rowTop = $el.offset().top;
+            maxHeight[rowIndex] = 0;
+        }
+        if ($el.height() > maxHeight[rowIndex]) {
+            maxHeight[rowIndex] = $el.height();
+        }
+        rows[rowIndex].push($el);
+    });
+	for (row = 1; row <= rowIndex; row++) {
+			for (i = 0; i <= rows[row].length; i++) {
+				$(rows[row][i]).height(maxHeight[row]);
+			}
+		}
+}
+$(window).on('resize load', equalHeights);
+
 if ($("#countdown").length > 0) {
   jQuery(document).ready(function(){	
     //var dateParts = $('#countdown').data("date") .match(/(\d+)-(\d+)-(\d+) (\d+):(\d+)/);
