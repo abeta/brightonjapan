07552c0d7b77b8ac2c6c54c5be751d7167d65e4d
koding user <adam@abemedia.co.uk>
2014-12-22 03:56:21 +0000

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,4 +5,5 @@ layout: nil
 {% include assets/js/bootstrap.min.js %}
 {% include assets/js/ekko-lightbox.min.js %}
 {% include assets/js/fittext.js %}
+{% include assets/js/parallax.js %}
 {% include assets/js/app.js %}
