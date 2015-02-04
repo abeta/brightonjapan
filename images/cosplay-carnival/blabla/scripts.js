379397b847aea1238ab9e53464eed280fe259d56
abeta <abeta@users.noreply.github.com>
2015-02-04 22:06:09 +0100

    Delete images/cosplay-carnival/blabla/scripts.js

--- images/cosplay-carnival/blabla/scripts.js
+++
@@ -1,8 +0,0 @@
----
-layout: nil
----
-{% include assets/js/jquery.min.js %}
-{% include assets/js/bootstrap.min.js %}
-{% include assets/js/ekko-lightbox.min.js %}
-{% include assets/js/fittext.js %}
-{% include assets/js/app.js %}
