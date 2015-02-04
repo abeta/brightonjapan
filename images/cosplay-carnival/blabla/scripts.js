9dd45f333ee091cf4e660cceaf00b855692960e5
abeta <abeta@users.noreply.github.com>
2015-02-04 22:05:34 +0100

    Uploade file scripts.js

---
+++ images/cosplay-carnival/blabla/scripts.js
@@ -0,0 +1,8 @@
+---
+layout: nil
+---
+{% include assets/js/jquery.min.js %}
+{% include assets/js/bootstrap.min.js %}
+{% include assets/js/ekko-lightbox.min.js %}
+{% include assets/js/fittext.js %}
+{% include assets/js/app.js %}
